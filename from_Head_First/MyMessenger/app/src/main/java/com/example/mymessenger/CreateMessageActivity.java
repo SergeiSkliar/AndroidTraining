5d4b9dc7d3e42a34886a46f7efd6cfc8ae3a0613
SergeiSkliar <sergei.skliar.work@gmail.com>
2024-02-29 17:04:38 +0500

    мессенджер из книги

---
+++ from_Head_First/MyMessenger/app/src/main/java/com/example/mymessenger/CreateMessageActivity.java
@@ -0,0 +1,24 @@
+package com.example.mymessenger;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+
+public class CreateMessageActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view) {
+        EditText ed = (EditText) findViewById(R.id.message);
+        String messageText = ed.getText().toString();
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        intent.putExtra(ReceiveMessageActivity.EXTRA_MESSAGE, messageText);
+        startActivity(intent);
+    }
+}
