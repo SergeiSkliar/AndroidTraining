2a1e8e549272ec54800a8391b53b451cf8eb22d9
SergeiSkliar <sergei.skliar.work@gmail.com>
2024-03-11 11:34:01 +0500

    stopwatch update

--- from_Head_First/Stopwatch/app/src/main/java/com/hfad/StopwatchActivity.java
+++ from_Head_First/Stopwatch/app/src/main/java/com/hfad/StopwatchActivity.java
@@ -13,13 +13,42 @@ import java.util.Locale;
 public class StopwatchActivity extends Activity {
     private int seconds = 0;
     private boolean running;
+    private boolean wasRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stopwatch);
 
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState.getInt("seconds");
+            running = savedInstanceState.getBoolean("running");
+            wasRunning = savedInstanceState.getBoolean("wasRunning");
+        }
+
         runTimer();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt("seconds", seconds);
+        savedInstanceState.putBoolean("running", running);
+        savedInstanceState.putBoolean("wasRunning", wasRunning);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        wasRunning = running;
+        running = false;
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        if (wasRunning) {
+            running = true;
+        }
     }
 
     public void onClickStart(View view){
