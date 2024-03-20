59015d4f22f3e9c87349201cae1d03c1d3439e5e
SergeiSkliar <sergei.skliar.work@gmail.com>
2024-03-20 16:59:19 +0500

    starbuzz upd

--- from_Head_First/Starbuzz/app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
+++ from_Head_First/Starbuzz/app/src/main/java/com/hfad/starbuzz/TopLevelActivity.java
@@ -1,14 +1,13 @@
 package com.hfad.starbuzz;
 
-import  androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class TopLevelActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_top_level);
     }
 }
