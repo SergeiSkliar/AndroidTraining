7f9d92b9db3baec96e1576c04d58fce3e68b7d06
SergeiSkliar <sergei.skliar.work@gmail.com>
2024-03-26 16:34:03 +0500

    starbuzz update

---
+++ from_Head_First/Starbuzz/app/src/main/java/com/hfad/starbuzz/DrinkCategoryActivity.java
@@ -0,0 +1,28 @@
+package com.hfad.starbuzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import android.content.Intent;
+import android.widget.AdapterView;
+
+public class DrinkCategoryActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drink_category);
+
+        ArrayAdapter<Drink> listAdapter = new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                Drink.drinks
+        );
+        ListView listDrinks = (ListView) findViewById(R.id.list_drinks);
+        listDrinks.setAdapter(listAdapter);
+
+        
+    }
+}
